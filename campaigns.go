9e380afd7456c2869c3beed631d5522b31f7fa88
Periweather <periweather.projects@gmail.com>
2018-08-10 23:50:49 -0400

    method stubs for campaigns

--- campaigns.go
+++ campaigns.go
@@ -1,1 +1,31 @@
 package tiltify
+
+// TODO according to the API, these don't just return the json object, but other stuff (meta) wrapped around it
+
+func GetCampaignById(campaignId int) Campaign {
+	return Campaign{}
+}
+
+func GetCampaignDonations(campaignId int) []Donation {
+	return nil
+}
+
+func GetCampaignRewards(campaignId int) []Reward {
+	return nil
+}
+
+func GetCampaignPolls(campaignId int) []Poll {
+	return nil
+}
+
+func GetCampaignChallenges(campaignId int) []Challenge {
+	return nil
+}
+
+func GetCampaignSchedule(campaignId int) Schedule {
+	return Schedule{}
+}
+
+func GetCampaignsSupportingCampaigns(campaignId int) []Campaign {
+	return nil
+}
