ea8ed24b01c7d8f3bce20fe3f6ffcc0cc088a363
Periweather <periweather.projects@gmail.com>
2018-08-10 23:41:05 -0400

    mostly empty testing stubs

---
+++ users_test.go
@@ -0,0 +1,31 @@
+package tiltify
+
+import "testing"
+
+func TestGetUser(t *testing.T) {
+
+}
+
+func TestGetUsers(t *testing.T) {
+
+}
+
+func TestGetUserById(t *testing.T) {
+
+}
+
+func TestGetCampaigns(t *testing.T) {
+
+}
+
+func TestGetCampaign(t *testing.T) {
+
+}
+
+func TestGetTeams(t *testing.T) {
+
+}
+
+func TestGetOwnedTeams(t *testing.T) {
+
+}
