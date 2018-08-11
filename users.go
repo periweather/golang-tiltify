582dcb81265de4391c99c9d4519f4117a8bea0eb
Periweather <periweather.projects@gmail.com>
2018-08-10 22:57:33 -0400

    stub for user calls, will probably change

--- users.go
+++ users.go
@@ -1,1 +1,29 @@
 package tiltify
+
+func GetUser() User {
+	return User{}
+}
+
+func GetUsers() []User {
+	return nil
+}
+
+func GetUserById(userId int) User {
+	return User{}
+}
+
+func GetCampaigns(userId int) []Campaign {
+	return nil
+}
+
+func GetCampaign(userId int, campaignId int) Campaign {
+	return Campaign{}
+}
+
+func GetTeams(userId int) []Team {
+	return nil
+}
+
+func GetOwnedTeams(userId int) []Team {
+	return nil
+}
